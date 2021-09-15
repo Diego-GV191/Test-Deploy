aac28a3f8c503170d0feb6ea5c8fe6a995c8e3ad
BADBOY.019 <forlan_vega@hotmail.com>
2021-09-15 12:28:11 -0500

    1.1

--- testDeploy/testDeploy/settings.py
+++ testDeploy/testDeploy/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5)s1wzm6%vpc3pg-+r!pm7-*yavovsq)08=iabcg_bew#31960'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
